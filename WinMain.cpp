1d720c40e7c85d3dcf8408a2ca798f2c6bd9999f
RunTuKillCha <859638516@qq.com>
2024-04-13 15:54:46 +0800

    第二次

--- WinMain.cpp
+++ WinMain.cpp
@@ -3,6 +3,14 @@ using namespace std;
 
 int main(){
 	cout<<"你干嘛，哎呦！"<<endl;
+	
+
+	cout<<"小黑子是吧"<<endl;
+	 cout<<"小黑子是吧"<<endl;
+	  cout<<"小黑子是吧"<<endl;
+	   cout<<"小黑子是吧"<<endl;
+	    cout<<"小黑子是吧"<<endl;
+
 
 	return 0;
 }
