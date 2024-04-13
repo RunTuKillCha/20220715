5670d479deb6dfdf1f429efb321be232c9f02145
RunTuKillCha <859638516@qq.com>
2024-04-13 15:41:35 +0800

    第一次提交

---
+++ WinMain.cpp
@@ -0,0 +1,94 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	cout<<"你干嘛，哎呦！"<<endl;
+
+	return 0;
+ rsa key pair.
+Enter passphrase (empty for no passphrase):
+Enter same passphrase again:
+Your identification has been saved in id_rsa.gitee
+Your public key has been saved in id_rsa.gitee.pub
+The key fingerprint is:
+SHA256:dvITmRmG8A00hZdonwu7csXQ4xkGf0YW9arjbpBDImI 859638516@qq.com
+ rsa key pair.
+Enter passphrase (empty for no passphrase):
+Enter same passphrase again:
+Your identification has been saved in id_rsa.gitee
+Your public key has been saved in id_rsa.gitee.pub
+The key fingerprint is:
+SHA256:dvITmRmG8A00hZdonwu7csXQ4xkGf0YW9arjbpBDImI 859638516@qq.com
+The key's randomart image is:
++---[RSA 3072]----+
+|      ..++...o.  |
+|       o==o o  . |
+|       .o*++    .|
+|    E . +.X=o  . |
+|   . . .SX*O  .  |
+|       ..+X. .   |
+|         oooo    |
+|      . o  o..   |
+|       o   oo    |
++----[SHA256]-----+
+
+85963@LAPTOP-8UIL3L2Q MINGW64 ~/Desktop/gitck (master)
+$ ssh-keygen -t rsa -C859638516@qq.com
+Generating public/private rsa key pair.
+Enter file in which to save the key (/c/Users/85963/.ssh/id_rsa):
+Created directory '/c/Users/85963/.ssh'.
+Enter passphrase (empty for no passphrase):
+Enter same passphrase again:
+Your identification has been saved in /c/Users/85963/.ssh/id_rsa
+Your public key has been saved in /c/Users/85963/.ssh/id_rsa.pub
+The key fingerprint is:
+SHA256:hadye9cpqyzNV8IdW35PNbNXYKntkJaX+MBg+IKx5jI 859638516@qq.com
+The key's randomart image is:
++---[RSA 3072]----+
+|         .     . |
+|      . ..o   +  |
+|       +.ooo B o |
+|      + .+. X =o+|
+|     o. S. o B =*|
+|    E .o .  o.*o=|
+|     o  .o. ooo.+|
+|        .oo..o  .|
+|         .oo.    |
++----[SHA256]-----+
+The key's randomart image is:
++---[RSA 3072]----+
+|      ..++...o.  |
+|       o==o o  . |
+|       .o*++    .|
+|    E . +.X=o  . |
+|   . . .SX*O  .  |
+|       ..+X. .   |
+|         oooo    |
+|      . o  o..   |
+|       o   oo    |
++----[SHA256]-----+
+
+85963@LAPTOP-8UIL3L2Q MINGW64 ~/Desktop/gitck (master)
+$ ssh-keygen -t rsa -C859638516@qq.com
+Generating public/private rsa key pair.
+Enter file in which to save the key (/c/Users/85963/.ssh/id_rsa):
+Created directory '/c/Users/85963/.ssh'.
+Enter passphrase (empty for no passphrase):
+Enter same passphrase again:
+Your identification has been saved in /c/Users/85963/.ssh/id_rsa
+Your public key has been saved in /c/Users/85963/.ssh/id_rsa.pub
+The key fingerprint is:
+SHA256:hadye9cpqyzNV8IdW35PNbNXYKntkJaX+MBg+IKx5jI 859638516@qq.com
+The key's randomart image is:
++---[RSA 3072]----+
+|         .     . |
+|      . ..o   +  |
+|       +.ooo B o |
+|      + .+. X =o+|
+|     o. S. o B =*|
+|    E .o .  o.*o=|
+|     o  .o. ooo.+|
+|        .oo..o  .|
+|         .oo.    |
++----[SHA256]-----+
+}
